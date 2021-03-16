db02187ee4cb1ccdd837c7a29ed36c30548b94e7
Yosh <62856297+yoshi2999@users.noreply.github.com>
2021-03-16 22:05:12 +0100

    change wrap to clamp

--- bti-fix-recursive.py
+++ bti-fix-recursive.py
@@ -6,11 +6,13 @@ colourenc = ['I4', 'I8', 'IA4', 'IA8', 'RGB565', 'RGB5A3', 'RGBA8', 0, 'CI4', 'C
 def fix(name):
     if not os.path.exists(name) or not os.path.isfile(name):
         return
-    if os.path.getsize(name) < 20:
+    if os.path.getsize(name) < 30:
         return
     with open(name, 'r+b') as bti:
         colour = bti.read(1)[0]
-        if 0 <= colour < 15 and colour not in [7, 11, 12, 13]:
+        bti.seek(0x10)
+        nooll = bti.read(12)
+        if 0 <= colour < 15 and colour not in [7, 11, 12, 13] and nooll[0:4] == b"\x00\x00\x00\x00" and nool[6:8] == b'\x00\x00' and nool[9:12] == b'\x00\x00\x00':
             bti.seek(6)
             bti.write(b'\x00\x00')
 
